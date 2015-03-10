d66533e5388008ecfe599fe1573eca5025938ab3
Gabor Mezo <gabor.mezo@swicon.hu>
2015-03-10 16:52:02 +0100

    sampler in progress

--- examples/jpeg-artifact-remover/trainer/sampler.js
+++ examples/jpeg-artifact-remover/trainer/sampler.js
@@ -7,6 +7,12 @@ var CachemanMemory = require("cacheman-memory");
 var Cacheman = require("cacheman");
 var Bluebird = require("bluebird");
 var lwip = require("lwip");
+var ref = require("ref");
+var uint32 = ref.types.uint32;
+
+function toRGBA(sourceColor) {
+    return (sourceColor.r << 24) | (sourceColor.g << 16) | (sourceColor.b << 8) || sourceColor.a;
+}
 
 function Sampler(files, options) {
     this.files = files;
@@ -18,7 +24,7 @@ Object.defineProperties(Sampler, {
         get: function () {
             return global.samplerCache ||
                 (global.samplerCache =
-                    Bluebird.promisifyAll(new Cacheman("samplerCache", { ttl: 60, engine: new CachemanMemory() })));
+                    Bluebird.promisifyAll(new Cacheman("samplerCache", {ttl: 60, engine: new CachemanMemory()})));
         }
     }
 });
@@ -29,16 +35,23 @@ Sampler.prototype.generateFFSample = task.async(function*() {
     let data = yield this._createSampleData(image, quality);
     const x = _.random(0, data.width - 1);
     const y = _.random(0, data.height - 1);
-    const key = `${image.path}_${quality}_${x}_${y}`;
-    let sample = yield Sampler.cache.getAsync(key);
-    if (!sample) {
-        return this._makeFFSample(data, x, y);
-    }
-    return sample;
+    return this._makeFFSample(data, x, y);
 });
 
-Sampler.prototype._makeFFSample = function(data, x, y) {
-
+Sampler.prototype._makeFFSample = function (data, x, y) {
+    let w2 = Math.floor(this.options.width / 2);
+    let h2 = Math.floor(this.options.height / 2);
+    let iBuff = new Buffer(this.options.width * this.options.height * 4);
+    let oBuff = new Buffer(4);
+    for (let sourceY = y - h2, destY = 0; destY < this.options.height; sourceY++, destY++) {
+        for (let sourceX = x - w2, destX = 0; destX < this.options.width; sourceX++, destX++) {
+            let iCoord = (destY * this.options.width * 4) + destX * 4;
+            let sourceColor = data.resultImage.getPixel(Math.abs(sourceX), Math.abs(sourceY));
+            let destColor = toRGBA(sourceColor);
+            uint32.set(iBuff, iCoord, destColor);
+        }
+    }
+    uint32.set(oBuff, 0, toRGBA(data.resultImage.getPixel(x, y)));
 };
 
 Sampler.prototype._createSampleData = task.async(function *(image, quality) {
@@ -50,7 +63,7 @@ Sampler.prototype._createSampleData = task.async(function *(image, quality) {
             width: image.width(),
             height: image.height(),
             originalImage: image,
-            resultImage: yield(lwip.openAsync(yield image.toBufferAsync("jpeg", { quality: quality }), "jpeg"))
+            resultImage: yield(lwip.openAsync(yield image.toBufferAsync("jpeg", {quality: quality}), "jpeg"))
         };
     }
     return data;
